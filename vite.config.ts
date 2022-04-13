eabf943f2fc75b6806a3f49ce8a7bf6576d02202
derlan <1372545550@qq.com>
2022-04-13 17:07:18 +0800

    feat:outDir

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'; 
 function pathResolve(dir: string) {
-  return resolve(process.cwd(), '.', dir);
+  return resolve(process.cwd(), '.', dir)
 }
+const outDir = '100'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -11,13 +12,16 @@ export default defineConfig({
     alias: [
       {
         find: /\/#\//,
-        replacement: pathResolve('types') + '/',
+        replacement: `${pathResolve('types')}/`,
       },
       {
         find: '@',
-        replacement: pathResolve('src') + '/',
+        replacement: `${pathResolve('src')}/`,
       },
-    ], 
+    ],
     dedupe: ['vue'],
   },
-})  
+  build: {
+    outDir,
+  },
+})
